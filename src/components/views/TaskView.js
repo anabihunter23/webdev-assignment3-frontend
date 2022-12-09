0737a119cf08917ca39df24a3466446e88e8d738
Aqib Nabi <Aqib.Nabi04@myhunter.cuny.edu>
2022-12-08 19:20:06 -0500

    finished tasks and employees containers and views using class code as reference, but trying to fix bug of employee name as undefined

---
+++ src/components/views/TaskView.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const TaskView = (props) => {
+  const { task } = props;
+  return (
+    <div>
+      <h1>{task.title}</h1>
+      <h2>
+        Task Priority: {task.priority}, Task Status: {task.completion}
+      </h2>
+      {task.employee ? (
+        <h3>{task.employee.firstname + " " + task.employee.lastname}</h3>
+      ) : (
+        <h3>employee</h3>
+      )}
+      <Link to={`/edittask/${task.id}`}>Edit task information</Link>
+      <br />
+      <Link to={`/tasks`}>View all tasks</Link>
+    </div>
+  );
+};
+
+export default TaskView;
