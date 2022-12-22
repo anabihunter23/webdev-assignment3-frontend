d26a79debdb9885ce0c941c0420dc5aa104925ba
Aqib Nabi <Aqib.Nabi04@myhunter.cuny.edu>
2022-12-21 19:05:38 -0500

    redux store setup for employee functions (add, delete, edit)

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import {
   TaskContainer,
   AllEmployeesContainer,
   AllTasksContainer,
+  NewEmployeeContainer,
   NewTaskContainer,
+  EditEmployeeContainer,
   EditTaskContainer,
 } from "./components/containers";
 
 // if you create separate components for adding/editing
-// a student or instructor, make sure you add routes to those
+// a student or employee, make sure you add routes to those
 // components here
 
 const App = () => {
@@ -23,10 +25,16 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/employees" component={AllEmployeesContainer} />
-        <Route exact path="/employee/:id" component={EmployeeContainer} />
         <Route exact path="/tasks" component={AllTasksContainer} />
-        <Route exact path="/newtask" component={NewTaskContainer} />
+        <Route exact path="/employee/:id" component={EmployeeContainer} />
         <Route exact path="/task/:id" component={TaskContainer} />
+        <Route exact path="/newemployee" component={NewEmployeeContainer} />
+        <Route exact path="/newtask" component={NewTaskContainer} />
+        <Route
+          exact
+          path="/editemployee/:id"
+          component={EditEmployeeContainer}
+        />
         <Route exact path="/edittask/:id" component={EditTaskContainer} />
       </Switch>
     </div>
